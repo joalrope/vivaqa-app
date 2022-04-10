0a63498b0385a12f8a7761b77dbcb609d0e643dd
Jose Rodriguez <joalrope@gmail.com>
2022-04-10 13:22:18 -0400

    front-end layout and router add

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,14 @@
-import { Provider } from 'react-redux';
-import { AppLayout } from './layouts/AppLayout';
-import { store } from './store/store';
+import { Provider } from "react-redux";
+import { AppLayout } from "./layouts/AppLayout";
+import { store } from "./store/store";
+import "antd/dist/antd.css";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <h1>HOLA MUNDO</h1>
-      <AppLayout />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<AppLayout />
+		</Provider>
+	);
 }
 
 export default App;
